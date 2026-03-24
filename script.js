d1f3461af7e363100000cb8dfa4e7973119ccfa7
Niniola Code <niniolaifeoluwa54@gmail.com>
2026-03-24 10:02:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function toggleMenu() {
-    document.querySelector(".nav-links").classList.toggle("show");
-}
 
-// Simple form alert
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Message sent successfully!");
-});
